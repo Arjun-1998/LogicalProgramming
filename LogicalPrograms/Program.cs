eb0d98343bb16f55c9c36e9aaf9f8e2ad83c0962
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-03 22:57:23 +0530

    [Arjun] Add ; Added Switched

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -9,7 +9,7 @@ namespace LogicalPrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect number\n 3. prime number\n 4. Reverse number\n 5. Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect number\n 3. prime number\n 4. Reverse number\n 5. coupon number\n 6. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -30,6 +30,12 @@ namespace LogicalPrograms
                         rev.ReverseNumber();
                         break;
                     case 5:
+                        Console.Write("How many numbers you wants to generate :");
+                        int N = Convert.ToInt32(Console.ReadLine());
+                        Logical cn = new Logical();
+                        cn.CouponNumber(N);
+                        break;
+                    case 6:
                         flag = false;
                         break;
                 }
